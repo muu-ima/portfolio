ba2cd0f96b62036c89e3d7e0cdd0da6eead5fbc3
muu-ima <im777bass@gmail.com>
2026-07-18 11:20:38 +0900

    Add loader and scroll top control

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import PortfolioControls from "./components/PortfolioControls";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -14,7 +15,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja" className="h-full antialiased">
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <PortfolioControls />
+        {children}
+      </body>
     </html>
   );
 }
