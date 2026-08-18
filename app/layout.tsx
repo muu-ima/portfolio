04b9c6fbc2747ccef81fa9db5709ec4cc0e54eac
muu-ima <im777bass@gmail.com>
2026-08-18 14:18:00 +0900

    Add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import PortfolioControls from "./components/PortfolioControls";
 import "./globals.css";
 
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <PortfolioControls />
         {children}
+        <Analytics />
       </body>
     </html>
   );
